ad948e92f78056c20fd750f31a0be1fca35cc4c6
Justin Stevens <justin.itrace@gmail.com>
2021-02-04 10:28:09 -0600

    initial commit

--- src/HitCounter.test.js
+++ src/HitCounter.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent } from '@testing-library/react';
-import HitCounter from "./index";
+import HitCounter from "./HitCounter";
 
 describe('on mount', () => {
   test('renders the button', () => {
